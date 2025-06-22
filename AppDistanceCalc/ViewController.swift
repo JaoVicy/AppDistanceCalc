64a68edc43fc70b4c12ac7c21d51f4a7f764fec7
JaoVicy <filipimjoa@gmail.com>
2025-06-22 18:57:17 -0300

    Initial Commit

---
+++ AppDistanceCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDistanceCalc
+//
+//  Created by João Víctor Benetti Filipim on 22/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppDistanceCalcTests/AppDistanceCalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppDistanceCalcTests.swift
+//  AppDistanceCalcTests
+//
+//  Created by João Víctor Benetti Filipim on 22/06/25.
+//
+
+import Testing
+@testable import AppDistanceCalc
+
+struct AppDistanceCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
