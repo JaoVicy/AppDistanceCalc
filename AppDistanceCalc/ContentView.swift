5a5c42111882f6b14c9a3d8ebd6769c7c3f909a4
JaoVicy <filipimjoa@gmail.com>
2025-06-27 20:57:37 -0300

    Created label Content View and ContentView class

---
+++ AppDistanceCalc/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AppDistanceCalc
+//
+//  Created by João Víctor Benetti Filipim on 27/06/25.
+//
+
+import UIKit
+
+class ContentView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
